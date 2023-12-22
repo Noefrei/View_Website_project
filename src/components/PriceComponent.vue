2ca3d218e9168e49da7877ffe7e5aff04ea26841
Noe <noe.frei@bluewin.ch>
2023-12-22 23:45:42 +0100

    Preis Komonente hinzugefügt

---
+++ src/components/PriceComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="price-container">
+    {{ formattedPrice }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      price: Math.floor(Math.random() * 301), // Zufälliger Preis zwischen 0 und 300 Fr.
+    };
+  },
+  computed: {
+    formattedPrice() {
+      return this.price.toFixed(2) + " Fr.";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.price-container {
+  font-size: 16px;
+  color: #e47911;
+  font-weight: bold;
+}
+</style>
+
+
+<style scoped>
+.price-container {
+  font-size: 16px;
+  color: #e47911; /* Beispiel für eine orangefarbene Preisfarbe */
+  font-weight: bold;
+}
+</style>
+
